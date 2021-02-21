d642fb26eeda97307d3b36b988083168ce49d484
amalshaji <amalshajid@gmail.com>
2021-02-21 06:09:50 +0530

    init

---
+++ cmd/gateway/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/fiber/adapter"
+	"github.com/gofiber/fiber/v2"
+)
+
+var fiberLambda *adapter.FiberLambda
+
+func init() {
+	// create a new fiber app
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"IP": c.IP(),
+		})
+	})
+	fiberLambda = adapter.New(app)
+}
+
+// Handler proxies our app requests to aws lambda
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return fiberLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	//
+	lambda.Start(Handler)
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/amalshaji/go-netlify
+
+go 1.16
+
+require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/aws/aws-lambda-go v1.22.0 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.9.0 // indirect
+	github.com/gofiber/fiber/v2 v2.5.0
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/valyala/fasthttp v1.21.0 // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+)
